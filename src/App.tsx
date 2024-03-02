50f56e825627e08a692ece0f3325d6e238d6f542
alexndrkovalev <yakovalev1@mail.ru>
2024-03-02 16:58:20 +0300

    add styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
-import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+	return <div className='App'>Hello, samurai! Let's go!</div>;
 }
-
 export default App;
